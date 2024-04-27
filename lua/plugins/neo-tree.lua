f03b43c108ae30069c770b3233d7499ba3a3aadb
Tom Harper <tom.harper@rareloop.com>
2024-04-27 18:36:45 +0100

    show hidden files and show nice icons

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -12,6 +12,23 @@ return {
 	},
 	opts = {
 		filesystem = {
+			follow_current_file = {
+				enabled = true, -- This will find and focus the file in the active buffer every time
+				--               -- the current file is changed while the tree is open.
+				leave_dirs_open = false, -- `false` closes auto expanded dirs, such as with `:Neotree reveal`
+			},
+			filtered_items = {
+				visible = true,
+				show_hidden_count = true,
+				hide_dotfiles = true,
+				hide_gitignored = true,
+				hide_by_name = {
+					-- '.git',
+					-- '.DS_Store',
+					-- 'thumbs.db',
+				},
+				never_show = {},
+			},
 			window = {
 				mappings = {
 					["\\"] = "close_window",
