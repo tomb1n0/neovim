d260db6476abac830e137a38c1fe8c5595229647
Tom Harper <tom.harper@rareloop.com>
2024-04-27 17:33:58 +0100

    Formatting

--- init.lua
+++ init.lua
@@ -1,21 +1,21 @@
 -- Core Keymaps
-require 'core.keymaps';
+require("core.keymaps")
 
 -- Install Lazy for Plugin Management
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable", -- latest stable release
+		lazypath,
+	})
 end
 vim.opt.rtp:prepend(lazypath)
 
 -- Setup Lazy, pointing it to our plugins folder.
 -- This way each plugin can have it's own file, making configuration a bit easier
 -- to manage in a modular way.
-require("lazy").setup('plugins', {})
+require("lazy").setup("plugins", {})
