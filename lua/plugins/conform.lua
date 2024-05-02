8fdaf6c4469a3561427e08b0139039f3e87210ee
Tom Harper <tom.harper@rareloop.com>
2024-05-02 16:19:58 +0100

    format PHP using prettier

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -31,6 +31,7 @@ return { -- Autoformat
 			-- You can use a sub-list to tell conform to run *until* a formatter
 			-- is found.
 			-- javascript = { { "prettierd", "prettier" } },
+			php = { "prettier" },
 		},
 	},
 }
