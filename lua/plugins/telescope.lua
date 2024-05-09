0144d86b569472df2e52b30308f2e39fc6f5896b
Tom Harper <tom.harper@rareloop.com>
2024-05-09 12:46:31 +0100

    fix not being able to search for brackets etc

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -74,7 +74,12 @@ return { -- Fuzzy Finder (files, lsp, etc)
 		-- vim.keymap.set("n", "<leader>ss", builtin.builtin, { desc = "[S]earch [S]elect Telescope" })
 		vim.keymap.set("n", "<leader>ss", builtin.lsp_document_symbols, { desc = "[S]earch [S]ymbols in File" })
 		vim.keymap.set("n", "<leader>sw", builtin.grep_string, { desc = "[S]earch current [W]ord" })
-		vim.keymap.set("n", "<leader>sG", builtin.live_grep, { desc = "[S]earch by [G]rep" })
+		vim.keymap.set(
+			"n",
+			"<leader>sG",
+			":lua require('telescope.builtin').live_grep({ additional_args = { '--fixed-strings' }})<CR>",
+			{ desc = "[S]earch by [G]rep" }
+		)
 		vim.keymap.set("n", "<leader>sd", builtin.diagnostics, { desc = "[S]earch [D]iagnostics" })
 		vim.keymap.set("n", "<leader>sr", builtin.resume, { desc = "[S]earch [R]esume" })
 		vim.keymap.set("n", "<leader>s.", builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
