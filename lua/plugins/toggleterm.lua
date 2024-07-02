daaaf02af1caad062a7c1963249ebbd39cc3a4db
Tom Harper <tom.harper@rareloop.com>
2024-07-02 13:23:20 +0100

    change keybind + use vertical

--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -3,9 +3,16 @@ return {
 	version = "*",
 	config = function()
 		require("toggleterm").setup({
-			open_mapping = [[<c-\>]],
+			size = function(term)
+				if term.direction == "horizontal" then
+					return 15
+				elseif term.direction == "vertical" then
+					return vim.o.columns * 0.4
+				end
+			end,
+			open_mapping = [[<c-`>]],
 			-- direction = 'vertical' | 'horizontal' | 'tab' | 'float',
-			direction = "float",
+			direction = "vertical",
 		})
 	end,
 }
